fb379d9be7e2a67524542501aaf068b762731ee2
Uhffection <142746146+Uhffection@users.noreply.github.com>
2026-05-23 16:23:29 -0700

    Establish getLarger function for determining which number is a larger integer

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 // Write your code below
+function getLarger(number1,number2){
+  if (number1 > number2)
+  console.log(number1);
+} else if (number2 > number1) {
+  console.log(number2);
+  return getLarger(number1,number2);
